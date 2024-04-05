b478fa399fe518b05809dd5c799d71884ae5bd14
devanshu <devanshugupta768@gmail.com>
2024-04-05 09:33:49 +0530

    Initial commit

---
+++ public class Calculator {.java
@@ -0,0 +1,21 @@
+public class Calculator {
+
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int subtract(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public int divide(int num1, int num2) {
+        if (num2 == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero!");
+        }
+        return num1 / num2;
+    }
+}
